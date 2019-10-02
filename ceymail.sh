0a760e2ce718766af39473d3f679c72ecb519641
Ceylavi <cey@ceylavi.com>
2019-10-02 00:28:25 +0000

    Update ceymail.sh

--- ceymail.sh
+++ ceymail.sh
@@ -2,6 +2,7 @@
 #Author: Ceylavi
 #Company: Ceylavi Technologies Inc. ©2019
 echo "CeyMail 1.0"
+#Hahahaha
 printf "Author: Ceylavi\n"
 printf "Company: Ceylavi Technologies Inc. ©2019\n"
 printf "support: cey@ceylavi.com\n"
